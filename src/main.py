b230f4ab227657e455ef0a5ba23bc304dbfcacb0
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2026-01-16 16:04:51 -0500

    CH2 - L2:TextNode Tests

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode(
+        "This is some anchor text",
+        TextType.LINK,
+        "https://www.boot.dev"
+    )
+    print(node)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        """Test that TextNode objects with same properties are equal"""
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_with_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.google.com")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_text_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
