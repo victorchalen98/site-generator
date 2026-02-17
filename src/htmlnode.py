26540201170bc145b26c50662ed90f7a2406e9a2
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2026-02-16 20:49:53 -0500

    CH2: Nodes - Completed

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from textnode import TextNode, TextType
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -46,4 +48,52 @@ class LeafNode(HTMLNode):
         html_str = f"{open_tag}{self.value}{close_tag}"          
         
         return html_str
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str | None, children: list[LeafNode], props: dict[str, str] | None = None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        
+        props_str = self.props_to_html()          
+        
+        open_tag = f"<{self.tag}{props_str}>"         
+        close_tag = f"</{self.tag}>"          
+        
+        html_str = f"{open_tag}"          
+        for child in self.children:             
+            html_str += child.to_html()          
+        
+        html_str += close_tag         
+        
+        return html_str
+    
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINK:
+            if text_node.url is None:
+                text_node.url = ""
+            return LeafNode(
+                tag="a", value=text_node.text, props={"href": text_node.url}
+            )
+        case TextType.IMAGE:
+            if text_node.url is None:
+                text_node.url = ""
+            return LeafNode(
+                tag="img", value="", props={"src": text_node.url, "alt": text_node.text}
+            )
+        case _:
+            raise Exception(f"Unknown TextNode type {text_node.text_type}")
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -34,6 +35,17 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
